703dbb7cc2b92473b4c75682240c5fcffa6db754
devgirlpro <agalidari@gmail.com>
2024-03-11 14:26:26 +0100

    understanding of Currying function

--- main.js
+++ main.js
@@ -41,3 +41,26 @@ fn()
 fn()
 fn()
 fn()
+
+//Function Currying
+//function f(a, b, c) is transformed to f(a)(b)(c)
+
+function sum(a, b, c) {
+    return a + b + c
+}
+
+console.log("sum(2, 3, 5) => ", sum(2, 3, 5))
+
+//sum(2, 3, 5) => sum(2)(3)(5)
+function curry(fn) {
+    return function(a) {
+        return function(b) {
+            return function(c) {
+                return fn(a, b, c)
+            }
+        }
+    }
+}
+
+const curriedSum = curry(sum)
+console.log("curriedSum =>", curriedSum(2)(3)(5))
