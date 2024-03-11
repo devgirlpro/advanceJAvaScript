9bf0d1be4b4ded84d4c849a2844ec6bbaa74936e
devgirlpro <agalidari@gmail.com>
2024-03-11 17:39:28 +0100

    callback function and higher order function

--- async.js
+++ async.js
@@ -4,7 +4,6 @@
 //3-  promises
 //4-  async await
 //5-  Event loop
- 
 
 //the traditional methods javaScript has available for running code asynchronously
 //setTimeout()  //setInterval()
@@ -14,30 +13,48 @@
 //setTimeout(function, duration, param1, param2, ...)
 
 function greet(name) {
-    console.log(`Heloo ${name} after 2 second`)
+  console.log(`Heloo ${name} after 2 second`);
 }
 
 // setTimeout(greet, 2000, 'lili');
 
-
 //to clear a timeout(), use the clearTimeout()
 const timeoutID = setTimeout(greet, 2000, 'lili');
-clearTimeout(timeoutID )
+clearTimeout(timeoutID);
 
-setTimeout(function run(name) {
-    console.log(`hello ${name}`)
-    setTimeout(run, 1000, 'Rosita')
-}, 1000, 'Rosi')
+//setTimeout act like setIntervals
+// setTimeout(function run(name) {
+//     console.log(`hello ${name}`)
+//     setTimeout(run, 1000, 'Rosi')
+// }, 1000, 'Rosi')
 
 //setInterval()
 //setInterval(function, duration, param1, param2, ...)
 
 function greet(name) {
-    console.log(`hele ${name} after each 2 seconds`)
+  console.log(`hele ${name} after each 2 seconds`);
 }
-
 
 //setInterval(greet, 2000, 'lili')
 //intervals keep runing a task forever=> should clear interval when appropriate
 const intervalId = setInterval(greet, 2000, 'lili');
-clearInterval(intervalId)
+clearInterval(intervalId);
+
+//Callbacks
+//1- function can be passt as a argument to a function
+//2- a function be return as avlaue from other functions
+
+//1-
+function greet(name) {
+  console.log(`hello ${name}`);
+}
+
+function greeting(greetFn) {
+  const name = 'lili';
+  greetFn(name);
+}
+
+greeting(greet);
+
+// any function that is passed as an argument to another function is called a callback function in js
+//the function which accepts a function as an argument or return a funcrion is called a higher order function
